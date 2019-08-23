961a14468c33220a35a07c46556321e3ae688068
Xinyue Li <xinyue.li@yale.edu>
2019-08-23 14:10:05 +0000

    version 1.0

---
+++ R/var3.R
@@ -0,0 +1,17 @@
+#' Covariate data for three individuals
+#'
+#' This is a synthetic data for 3 individuals, and the variables are "ID", age" and "gender". It is for
+#' illustration purposes of the functions in the package \code{PML} only.
+#'
+#' @docType data
+#'
+#' @usage data(pa3)
+#'
+#' @format An object of class \code{data.frame}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(var3)
+#'   
+#' @seealso \code{\link{tre}}
